0e2381f40fcb1e25bd9dea8981bc2cb865797bc1
azzuazu <uedzeruch@gmail.com>
2022-06-07 00:09:29 +0900

    WUP

--- src/main.py
+++ src/main.py
@@ -1,5 +1,39 @@
 import pyautogui
+import time
 
-p = pyautogui.locateOnScreen("./img/CoE_all_skill.png")
-x, y = pyautogui.center(p)
-pyautogui.click(x, y)
+
+# 実行時間計測
+start = time.time()
+
+# ImageNotFoundException::指定の画像が見つからなかった場合、これが出るらしいが、
+# None が返ってきた
+
+# ベースをチェック
+base = pyautogui.locateOnScreen("./img/base_marble.png")
+print(base)
+if base == None:
+    print("ベースアイテムが見つかりません")
+else:
+    # ベースの中心座標を取る
+    bx, by = pyautogui.center(base)
+    print("ベースアイテム発見伝！")
+
+    # ほしいmodを探す region=(x,y,width,height)
+    mod = None
+    i = 0
+    while mod == None and i < 10000:
+        mod = pyautogui.locateOnScreen("./img/CoE_all_fire_skill.png", region=(250,400,417,290), grayscale=True)
+        print(mod)
+
+        if mod != None:
+            print("breakします")
+            break
+        else: 
+            print("clickします")
+            pyautogui.click(bx, by)
+        
+        i = i + 1
+        print(i)
+    end = time.time()
+    dur = end - start
+    print("クラフト終了！ 経過時間:{:.1f}".format(dur) + " [sec]")
